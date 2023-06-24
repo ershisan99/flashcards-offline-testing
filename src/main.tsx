b6f04681e3a7f4dd50ee9fc7470e252c68d129e9
andres <andres@doersdf.com>
2023-06-24 12:47:54 +0200

    feat: add styles

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import './styles/index.scss'
+import { StrictMode } from 'react'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import { createRoot } from 'react-dom/client'
+
+import { App } from './App.tsx'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
